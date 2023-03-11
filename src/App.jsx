b9c4734ff8161a59108353a21ab92d003413edc5
PrismaIllyaDev <malopez091@gmail.com>
2023-03-10 18:54:49 -0700

    tercer commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
+import { useState } from "react";
+import {
+  RiMenuLine,
+  RiAliensLine,
+  RiAddFill,
+  RiShoppingBag3Line,
+  RiCloseFill
+} from "react-icons/ri";
+//Component
 import Sidebar from "./components/shared/Sidebar.jsx";
 
-
-
-
 function App() {
-  return <div className="bg-[#353941] w-full min-h-screen"><Sidebar/></div>;
+  const [showMenu, setShowMenu] = useState(false);
+  const [showOrder, setShowOrder] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  return (
+    <div className="bg-[#353941] w-full min-h-screen">
+      <Sidebar showMenu={showMenu}/>
+      {/* Movile Menu */}
+      <nav className="bg-[#80161f] lg:hidden fixed w-full bottom-0 left-0 text-xl text-gray-400 py-2 px-8 flex items-center justify-between rounded-tl-xl rounded-tr-xl">
+        <button className="p-2">
+          <RiAliensLine />
+        </button>
+        <button className="p-2">
+          <RiAddFill />
+        </button>
+        <button className="p-2">
+          <RiShoppingBag3Line />
+        </button>
+        <button onClick={toggleMenu} className="p-2">
+          {showMenu ? <RiCloseFill/> : <RiMenuLine />}
+        </button>
+      </nav>
+    </div>
+  );
 }
 
 export default App;
--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -1,19 +1,80 @@
 import React from "react";
-import { RiStore2Line } from "react-icons/ri";
+import {
+  RiStore2Line,
+  RiShoppingBag3Line,
+  RiMailLine,
+  RiNotification2Line,
+  RiSettings3Line,
+  RiLogoutBoxRLine,
+} from "react-icons/ri";
 
-const Sidebar = () => {
+const Sidebar = (props) => {
+  const { showMenu } = props;
+
   return (
-    <div className="bg-[#80161f] fixed left-0 top-0 w-28 h-full">
-      <h1 className="text-2xl text-gray-300 uppercase font-bold text-center my-5">
-        LOGO
-      </h1>
-      <ul className="pl-4">
-        <li className="bg-[#353941] p-4 rounded-tl-xl rounded-bl-xl">
-          <a href="#" className="bg-[#80161f] p-4 block rounded-xl text-white">
-            <RiStore2Line className="text-2xl "/>
-          </a>
-        </li>
-      </ul>
+    <div
+      className={`bg-[#80161f] fixed lg:left-0 transition-all top-0 w-28 h-full flex flex-col justify-between py-6 rounded-br-xl rounded-tr-xl z-50 
+      ${showMenu ? "left-0" : "-left-full"}`}
+    >
+      <div>
+        <ul className="pl-4">
+          <h1 className="text-2xl text-gray-300 uppercase font-bold text-center my-5">
+            LOGO
+          </h1>
+          <li className="bg-[#353941] p-4 rounded-tl-xl rounded-bl-xl">
+            <a
+              href="#"
+              className="bg-[#80161f] p-4 flex justify-center rounded-xl text-white"
+            >
+              <RiStore2Line className="text-2xl " />
+            </a>
+          </li>
+          <li className="hover:bg-[#353941] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+            <a
+              href="#"
+              className="bg-[#80161f] p-4 flex justify-center rounded-xl group-hover:text-white transition-colors"
+            >
+              <RiShoppingBag3Line className="text-2xl " />
+            </a>
+          </li>
+          <li className="hover:bg-[#353941] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+            <a
+              href="#"
+              className="bg-[#80161f] p-4 flex justify-center rounded-xl group-hover:text-white transition-colors"
+            >
+              <RiMailLine className="text-2xl " />
+            </a>
+          </li>
+          <li className="hover:bg-[#353941] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+            <a
+              href="#"
+              className="bg-[#80161f] p-4 flex justify-center rounded-xl group-hover:text-white transition-colors"
+            >
+              <RiNotification2Line className="text-2xl " />
+            </a>
+          </li>
+          <li className="hover:bg-[#353941] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+            <a
+              href="#"
+              className="bg-[#80161f] p-4 flex justify-center rounded-xl group-hover:text-white transition-colors"
+            >
+              <RiSettings3Line className="text-2xl " />
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <ul className="pl-4">
+          <li className="hover:bg-[#353941] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+            <a
+              href="#"
+              className="bg-[#80161f] p-4 flex justify-center rounded-xl group-hover:text-white transition-colors"
+            >
+              <RiLogoutBoxRLine className="text-2xl " />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
