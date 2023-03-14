f8c2ecde2b9064feaac235bacae5ac88433b554d
PrismaIllyaDev <malopez091@gmail.com>
2023-03-13 19:38:40 -0700

    creo que es el cuarto push

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import {
   RiAliensLine,
   RiAddFill,
   RiShoppingBag3Line,
-  RiCloseFill
+  RiCloseFill,
+  RiSearch2Line,
 } from "react-icons/ri";
 //Component
 import Sidebar from "./components/shared/Sidebar.jsx";
@@ -19,7 +20,7 @@ function App() {
 
   return (
     <div className="bg-[#353941] w-full min-h-screen">
-      <Sidebar showMenu={showMenu}/>
+      <Sidebar showMenu={showMenu} />
       {/* Movile Menu */}
       <nav className="bg-[#80161f] lg:hidden fixed w-full bottom-0 left-0 text-xl text-gray-400 py-2 px-8 flex items-center justify-between rounded-tl-xl rounded-tr-xl">
         <button className="p-2">
@@ -32,9 +33,52 @@ function App() {
           <RiShoppingBag3Line />
         </button>
         <button onClick={toggleMenu} className="p-2">
-          {showMenu ? <RiCloseFill/> : <RiMenuLine />}
+          {showMenu ? <RiCloseFill /> : <RiMenuLine />}
         </button>
       </nav>
+      <main className="lg:pl-28 grid grid-cols-1 lg:grid-cols-8">
+        <div className="lg:col-span-6">
+          {/*Header*/}
+          <header className="p-4">
+            {/*Title and Search*/}
+            <div className="flex flex-col gap-4 mb-6">
+              <div>
+                <h1 className="text-2xl text-gray-300">Aerospace Gift Shop</h1>
+                <p className="text-gray-500">13 Marzo 2023</p>
+              </div>
+              <form>
+                <div className="w-full relative">
+                  <RiSearch2Line className="absolute left-1 top-1/2 -translate-y-1/2 text-gray-300" />
+                  <input
+                    placeholder="Search"
+                    className="bg-gray-600 w-full py-2 pl-10 pr-4 rounded-lg text-gray-300 outline-none"
+                    type="text"
+                  />
+                </div>
+              </form>
+            </div>
+            {/*Tabs*/}
+            <nav className="text-gray-300 flex items-center justify-between border-b">
+              <a
+                href=""
+                className="relative py-2 pr-4 text-[#80161f] before:w-1/2 before:h-[2px] before:absolute before:bg-[#80161f] before:left-0 before:rounded-full before:-bottom-[1px]"
+              >
+                Aerospace
+              </a>
+              <a href="" className=" py-2 pr-4">
+                Gifts
+              </a>
+              <a href="" className=" py-2 pr-4">
+                Food
+              </a>
+              <a href="" className=" py-2">
+                Tech
+              </a>
+            </nav>
+          </header>
+        </div>
+        <div className="lg:col-span-2 fixed lg:static right-0">Carrito</div>
+      </main>
     </div>
   );
 }
